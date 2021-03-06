2340fd14f579382d736c83f7b631de1d2bf9ea47
Switch24-7 <stephenjusto24@gmail.com>
2021-03-05 17:26:04 -0800

    Login screen draft

--- App.js
+++ App.js
@@ -19,37 +19,31 @@ export default function App() {
         <Stack.Screen 
           name="Login"
           component={LoginScreen}
-          options={{title: 'LoginScreen'}}
+          options={{headerShown: false}}
         />
         <Stack.Screen 
           name="Signup"
           component={SignupScreen}
-          options={{title: 'SignupScreen'}}
         />
         <Stack.Screen 
           name="Main"
           component={MainScreen}
-          options={{title: 'MainScreen'}}
         />
         <Stack.Screen 
           name="Flashcards"
           component={FlashcardsScreen}
-          options={{title: 'FlashcardsScreen'}}
         />
         <Stack.Screen 
           name="Account"
           component={AccountScreen}
-          options={{title: 'AccountScreen'}}
         />
         <Stack.Screen 
           name="Courses"
           component={CoursesScreen}
-          options={{title: 'CoursesScreen'}}
         />
         <Stack.Screen 
           name="CoursePlanner"
           component={CoursePlannerScreen}
-          options={{title: 'CoursePlannerScreen'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ app/config/colors.js
@@ -0,0 +1,10 @@
+export default {
+    black: '#000',
+    bone: '#D8D0C1',
+    dimGray: '#6F686D',
+    languidLavender: '#E0D3DE',
+    lightGray: '#f7f7f7',
+    paleSilver: '#CBB8A9',
+    sage: '#B3B492',
+    white: '#fff'
+}
