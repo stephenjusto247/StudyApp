d9c7d35d19dd65d2adf8ff31d1c0a45b2d155668
Switch24-7 <stephenjusto24@gmail.com>
2021-03-07 11:21:25 -0800

    Styling FlashcardStudyScreen.js

--- client/app/screens/FlashcardStudyScreen.js
+++ client/app/screens/FlashcardStudyScreen.js
@@ -8,10 +8,12 @@ import {
   ScrollView,
   Dimensions,
 } from "react-native";
+import colors from "../config/colors";
 
 export default function FlashcardStudyScreen(props) {
   const [flashcardSet, setFlashcardSet] = React.useState({});
   const [show, setShow] = React.useState(false);
+
   React.useEffect(() => {
     if (props.route.params) {
       const newFlashcard = {
@@ -22,24 +24,13 @@ export default function FlashcardStudyScreen(props) {
     }
   }, [props.route.params]);
 
-  let screenWidth = Dimensions.get("window").width;
-  let screenHeight = Dimensions.get("window").height;
-
   const list = () => {
     if (flashcardSet.flashcards !== undefined) {
       return flashcardSet.flashcards.map((entry, index) => {
         return (
           <View
             key={index}
-            style={{
-              backgroundColor: "#fff",
-              flex: 1,
-              width: screenWidth,
-              height: screenHeight,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
+            style={styles.flashcard}>
             <Text
               style={{
                 fontSize: 25,
@@ -55,12 +46,6 @@ export default function FlashcardStudyScreen(props) {
                 title="Show/Hide Answer"
                 onPress={() => setShow(!show)}
               ></Button>
-              <Button
-                title="Back"
-                onPress={() => {
-                  props.navigation.navigate("FlashcardSetScreen", flashcardSet);
-                }}
-              />
             </Text>
           </View>
         );
@@ -69,14 +54,76 @@ export default function FlashcardStudyScreen(props) {
   };
 
   return (
-    <ScrollView
-      horizontal={true}
-      pagingEnabled={true}
-      onMomentumScrollBegin={() => {
-        setShow(false);
-      }}
-    >
-      {list()}
-    </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.headerSection}>
+        <Text style={styles.header}>{flashcardSet.set}</Text>
+      </View>
+      <View style={styles.mainSection}>
+        <ScrollView
+          horizontal={true}
+          pagingEnabled={true}
+          onMomentumScrollBegin={() => {
+            setShow(false);
+          }}
+        >
+          {list()}
+        </ScrollView>
+      </View>
+      <View style={styles.bottomSection}>
+      <TouchableOpacity style={styles.back} onPress={() => {props.navigation.navigate("FlashcardSetScreen", flashcardSet)}}>
+          <Text style={styles.backText}>Back</Text>
+      </TouchableOpacity>
+      </View>
+    </View>
   );
 }
+
+const styles= StyleSheet.create({
+  back:{
+    alignItems: 'center',
+    borderColor: colors.paleSilver,
+    borderWidth: 2,
+    borderRadius: 10,
+    justifyContent: 'center',
+    marginTop: 10,
+    height: 45,
+    width: 100
+  },
+  backText: {
+    color: colors.paleSilver,
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  bottomSection: {
+    flex: .25,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  container: {
+    flex: 1,
+    paddingTop: '15%',
+    backgroundColor: colors.white
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  headerSection: {
+    flex: .25,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  flashcard: {
+    backgroundColor: "#fff",
+    flex: 1,
+    width: Dimensions.get("window").width,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  flashcards: {
+    flex: 1
+  },
+  mainSection: {
+    flex: .5
+  },
+});
