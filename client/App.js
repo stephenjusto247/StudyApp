990a78d38b1be2a20cfa68a514700bd0d4f9149d
Switch24-7 <stephenjusto24@gmail.com>
2021-03-07 11:02:26 -0800

    Fixed feature-flashcard files

--- client/App.js
+++ client/App.js
@@ -12,7 +12,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen
           name="Login"
           component={LoginScreen}
